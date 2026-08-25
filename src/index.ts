922e7f7e9d05216efb2c6e7cf5538e8f7e67da80
devsdaddy <start@ncommx.com>
2026-08-25 13:06:19 +0500

    Add main

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+/**
+ * TypeScript Coroutines Library
+ *
+ * @git             https://github.com/devsdaddy/quarkdash
+ * @version         1.2.0
+ * @author          Elijah Rastorguev
+ * @build           1024
+ * @website         https://dev.to/devsdaddy
+ * @updated         24.08.2026
+ */
+/* Export components */
+export {Arena, PooledArena, type IArena} from "./core/arena";
+export {Pool, createPool, type PoolFactory, type IPooled} from "./core/pool";
+export {StackFrame, FrameStack, FrameState} from "./core/frame";
+export {BinaryHeap, type HeapComparator} from "./utils/heap";
+export {nowMs, hrtimeMs} from "./utils/time";
+export {
+    DEFAULT_PRIORITY_CONFIG,
+    DEFAULT_DECAY_OPTIONS,
+    clampPriority,
+    DecayPriorityStrategy,
+    FixedPriorityStrategy,
+    PriorityEntry,
+    comparePriority,
+    type PriorityConfig,
+    type DecayOptions,
+    type IPriorityStrategy,
+    type PriorityLevel,
+} from "./core/priority";
+export {
+    EFFECT_TYPE,
+    yieldMain,
+    sleep,
+    fork,
+    cancel,
+    all,
+    race,
+    call,
+    awaitPromise,
+    yieldEvery,
+    setPriority,
+    isEffect,
+    type Effect,
+    type CoroutineFactory,
+    type YieldEffect,
+    type SleepEffect,
+    type ForkEffect,
+    type AllEffect,
+    type RaceEffect,
+} from "./core/effects";
+export {
+    Coroutine,
+    CancelError,
+    createHandle,
+    CoroutineState,
+    isAsyncGenerator,
+    type CoroutineHandle,
+    type CoroutineOptions,
+    type AnyGenerator,
+} from "./core/coroutine";
+export {Scheduler, createScheduler, type SchedulerOptions, type SchedulerStats} from "./core/scheduler";
+export {
+    DefaultEventLoopAdapter,
+    ManualEventLoopAdapter,
+    getYieldFn,
+    resetYieldCache,
+    type EventLoopAdapter,
+    type YieldFn,
+} from "./runtime/event-loop";
+export {
+    DistributedScheduler,
+    WorkerChannel,
+    createDistributedScheduler,
+    type WorkerPoolOptions,
+} from "./runtime/worker-pool";
+export {Channel, Semaphore, slidingBuffer, droppingBuffer, fixedBuffer, type ChannelBuffer} from "./core/channel";
+export {WasmArena} from "./core/wasm.arena";
+export {AtomicHeap} from "./utils/atomic.heap";
+export {Tracer, type TraceEvent} from "./utils/tracing";
+export {WorkStealingPool, Deque, type StealableQueue} from "./core/work-stealing";
